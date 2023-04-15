c9b2a59af3b60fb5c4434b8b0abef28eb4e6568d
yeffreycode <yeffreyespinoza200@gmail.com>
2023-04-15 04:22:53 -0500

    remove port in next config for images remotes

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         protocol: process.env.NEXT_PUBLIC_SERVER_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_SERVER_HOSTNAME,
-        port: "8080",
+        port: "",
         pathname: "/public/**",
       },
     ],
